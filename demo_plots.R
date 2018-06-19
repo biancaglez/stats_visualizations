bad4f723a2f7b98654c66e5cc25b7d967aa6e9df
biancaglez <bianca.glez94@gmail.com>
2018-06-19 08:24:06 -0600

    updating the demo_plots fn

--- demo_plots.R
+++ demo_plots.R
@@ -3,19 +3,21 @@
 # # facet_grid(~sex) +
 
 # initialize data - will have to read the csv and define list of variables interested in (demographics) etc
+
 demo_plots <- function(private) { 
-  
 #########################################################################################################################
 # this demographic vector showcases what we are usually interested in   
 demo_vec<- c("gender","household_income","favm","number_of_children",
-             "marital_status","age_cats","education","occupation_type","beds","number_of_rooms")  
+             "marital_status","education","beds","number_of_rooms")  
+
+# select these dem_vec variables and IDs
+private<-private %>% select(demo_vec,id)
+
 na.list<-list() 
 listy<-list()
 temp_df<-data.frame()
 plot_list<-list()
-
-# select these dem_vec variables and IDs
-private<-private %>% select(demo_vec,id)
+char_plots<-list()
 
 for(i in 1:length(demo_vec)){
   
@@ -33,20 +35,30 @@ for(i in 1:length(demo_vec)){
   
   names(temp_df2)[1] <- temp_names
   
-  #plot thing
-  p<-ggplot(data=temp_df2, aes_string(x= colnames(temp_df2)[1], y = colnames(temp_df2)[3])) + geom_bar(stat='identity')+xlab(colnames(temp_df2)[1])+ylab(colnames(temp_df2)[3])+ggtitle(paste(colnames(temp_df2)[1],"percentage")) + scale_x_continuous(labels = scales::comma) + theme(axis.text.x = element_text(angle = 40, hjust = 1))
-  plot_list[[i]] = p
- 
+  # plot things 
   
+  if (is.character(temp_df2[[1]]) == TRUE) {
+   
+    p<-ggplot(data=temp_df2, aes_string(x= colnames(temp_df2)[1], y = colnames(temp_df2)[3])) + geom_bar(stat='identity')+xlab(colnames(temp_df2)[1])+ylab(colnames(temp_df2)[3])+ggtitle(paste(colnames(temp_df2)[1],"percentage")) + theme(axis.text.x = element_text(angle = 40, hjust = 1))+ scale_x_discrete()
+    char_plots[[i]] = p
+    
+  } else {
+    p<-ggplot(data=temp_df2, aes_string(x= colnames(temp_df2)[1], y = colnames(temp_df2)[3])) + geom_bar(stat='identity')+xlab(colnames(temp_df2)[1])+ylab(colnames(temp_df2)[3])+ggtitle(paste(colnames(temp_df2)[1],"percentage")) + theme(axis.text.x = element_text(angle = 40, hjust = 1))+ scale_x_continuous(labels = scales::comma) 
+    plot_list[[i]] = p
+  }
+  # R functions don't return multiple objects
+  #return(plot_list)
+  
+  }
 }
 
-# R functions don't return multiple objects
-return(plot_list)
-}
+# okay so doenst want to assign within the forloop so whatever
+str(char_plots)
+plot_list[[1]] # + scale_x_discrete()
+
 
 # customize certain plots - household income
 hh_plot<-plot_list[[2]]
-hh_plot
 hh_plot + scale_x_continuous(name="Household Income", limits=c(0, 200000)) + ggtitle("Household Income % Breakdown") + scale_x_continuous(labels = scales::dollar)+xlab("Household Income") + scale_y_continuous(labels = scales::percent)
 
 
